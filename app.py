95bb326b5a1ffefb0c91b530acc1d1c18c7b17dd
Ravikiran Rao <rkr4lg3d@gmail.com>
2021-11-12 19:29:22 +0530

    Part-7VideoChanges

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 import binance_keys as keys
 import csv
 from binance import Client
@@ -6,6 +6,7 @@ from binance.enums import *
 
 app = Flask(__name__)
 client = Client(keys.BINANCEONE_KEY, keys.BINANCEONE_SECRET)
+app.secret_key = b'bjkdslfdjkbinancekdlfjskfsjdl'
 
 @app.route('/')
 def index():
@@ -13,25 +14,31 @@ def index():
     info = client.get_account()
     balances = info['balances']
     # print(balances)
-    return render_template('index.html', title=title, my_balances=balances)
+    exchange_info = client.get_exchange_info()
+    # print(exchange_info['symbols'])
+    return render_template('index.html', title=title, my_balances=balances, symbols=exchange_info['symbols'])
     
 @app.route('/watchlist')
 def watchlist():
     prices = client.get_all_tickers()
-    print(prices)
+    # exchange_info = client.get_exchange_info()
+    # print(prices)
     return render_template('watchlist.html', list=prices)
         
-@app.route('/buy')
+@app.route('/buy', methods = ['POST'])
 def buy():
-    order = client.create_order(
-        symbol='LTC',
-        side=SIDE_BUY,
-        type=ORDER_TYPE_LIMIT,
-        timeInForce=TIME_IN_FORCE_GTC,
-        quantity=100,
-        price='0.00001')  
+    # print('Buying')
+    print(request.form)
+    try:
+        order = client.create_order(
+            symbol=request.form['symbol'],
+            side=SIDE_BUY,
+            type=ORDER_TYPE_MARKET,
+            quantity=request.form['quantity'])
+    except Exception as e:
+            flash(e.message, "error")
 
-    return 'buy'
+    return redirect('/')
 
 @app.route('/sell')
 def sell():
