8daff496f20a0624e338268218957f9ba6ea309e
Ravikiran Rao <rkr4lg3d@gmail.com>
2021-11-13 16:07:13 +0530

    Part8 Video Realtime charts using javascript and websockets

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, jsonify
+from flask_cors import CORS, cross_origin
 import binance_keys as keys
 import csv
 from binance import Client
 from binance.enums import *
 
 app = Flask(__name__)
+CORS(app)
 client = Client(keys.BINANCEONE_KEY, keys.BINANCEONE_SECRET)
 app.secret_key = b'bjkdslfdjkbinancekdlfjskfsjdl'
 
@@ -47,3 +49,23 @@ def sell():
 @app.route('/settings')
 def settings():
     return 'settings'
+
+@app.route('/history')
+def history():
+    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_5MINUTE, "1 Nov, 2021", "13 Nov, 2021")
+
+    # print(type(candlesticks[0]))
+
+    processed_candlesticks = []
+
+    for data in candlesticks:
+        candlestick = {
+             'time': data[0]/1000, 
+             'open': data[1], 
+             'high': data[2], 
+             'low': data[3], 
+             'close': data[4] }
+        processed_candlesticks.append(candlestick)
+
+    return jsonify(processed_candlesticks)
+    
